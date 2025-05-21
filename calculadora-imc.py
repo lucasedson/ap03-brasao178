16c450681e758966418aa334a6dc9ac5aeb24e41
Lucas Edson <lucasedson654@gmail.com>
2025-05-21 20:48:48 -0300

    atualiza imc

--- calculadora-imc.py
+++ calculadora-imc.py
@@ -1,6 +1,20 @@
 def imc(massa:float, altura:float):
-    imc = massa / (altura ** 2)
+    imc = float(massa) / (float(altura) ** 2)
 
-    if imc < 18.5 and imc < 25:
-        "Abaixo do Peso"
+    if imc < 18.5:
+        return "Abaixo do Peso"
+    elif imc < 25:
+        return "Peso Normal"
+    elif imc < 30:
+        return "Sobrepeso"
+    else:
+       return "Obeso"
     
+if __name__ == "__main__":
+    while True:
+        print("Calculadora de IMC!")
+        peso = input("Digite o peso: ")
+        altura = input("Digite a altura: ")
+
+        print(imc(peso, altura))
+        input("Aperte enter para continuar ou ctrl+c para sair")
